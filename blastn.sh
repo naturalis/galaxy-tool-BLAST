f5e6b2ca66c9b08ab6c225972712b5471ec3e676
MartenHoogeveen <martenhoogeveen@gmail.com>
2018-06-20 10:40:59 +0200

    Dynamic fixes

--- blastn.sh
+++ blastn.sh
@@ -3,6 +3,7 @@
 #outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
 #location for the testserver
 #outlocation=$(mktemp -d /media/GalaxyData/files/XXXXXX)
+
 outlocation=$(mktemp -d /home/galaxy/ExtraRef/XXXXXX)
 blastn_wrapper.py -it $1 -i $2 -db $3 -bt $4 -bm $5 -tl $6 -of $outlocation -outfmt $7
 
--- test_tool_form_utils.py
+++ test_tool_form_utils.py
@@ -1,9 +1,38 @@
-def get_field_components_options(taxidlist):
+def taxon_filter(database):
+    options = [("None","none", 1)]
+    database = [i.encode('utf-8') for i in database]
+    for x in database:
+        if "v5" not in str(x).lower():
+            options = [("None","none", 1)]
+            break
+        else:
+            options.append(("Actinopterygii", "/home/ubuntu/testmapMarten/test/Marten/github_scripts/galaxy-tool-BLAST/database/Actinopterygii_taxidlist",1))
+    return options
 
-    with open("/home/ubuntu/test.txt", "a") as testfile:
-        testfile.write(taxidlist)
+def taxonomy_source_list(database):
+    options = [("None","none", 1)]
+    options_dict = {}
+    options_dict["NCBI"] = ("NCBI", "NCBI", 1)
+    options_dict["BOLD"] = ("BOLD", "BOLD", 1)
+    options_dict["GBIF"] = ("GBIF", "GBIF", 1)
+    options_dict["default"] = ("Original taxonomy of the database", "default", 1)
+    mem = []
+    database = [i.encode('utf-8') for i in database]
+    for x in database:
+        if "v5" in str(x).lower():
+            mem.append("NCBI")
+        if "bold" in str(x).lower():
+            mem.append("BOLD")
 
-    options = [("hoi", "hoi", 2),("hallo", "hallo", 2)]
-    if taxidlist != "none":
-        options.append(("actino", taxidlist ,3))
+    if "NCBI" in mem and "BOLD" in mem:
+        options_dict.pop("NCBI")
+        options_dict.pop("BOLD")
+    elif "NCBI" in mem and "BOLD" not in mem:
+        options_dict.pop("BOLD")
+    elif "NCBI" not in mem and "BOLD" in mem:
+        options_dict.pop("NCBI")
+
+    for y in options_dict:
+        options.append(options_dict[y])
+
     return options
