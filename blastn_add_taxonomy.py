5ada337a59ed9a33172c4d0b0da30e7890cb1429
MartenHoogeveen <martenhoogeveen@gmail.com>
2018-06-21 15:42:38 +0200

    Added gbif

--- blastn_add_taxonomy.py
+++ blastn_add_taxonomy.py
@@ -3,7 +3,7 @@
 blastn_add_taxonomy   V1.0    martenhoogeveen@naturalis.nl
 This script adds the taxonomy to the BLAST output. The input is de folder path that contains the blast results.
 """
-import json, sys, argparse, os, glob
+import json, sys, argparse, os, glob, requests
 # Retrieve the commandline arguments
 parser = argparse.ArgumentParser(description='Add taxonomy to BLAST output')
 parser.add_argument('-i', '--blast_input_folder', metavar='input folder with BLAST custom outfmt 6 output', dest='blastinputfolder', type=str,help='input folder', required=True)
@@ -60,18 +60,112 @@ def find_genbank_taxonomy(hit, taxonomyDict, mergedTaxonDict):
         else:
             return hit.strip() + "\t" + taxonomydb[taxid]["kingdom"] + " / " + taxonomydb[taxid]["phylum"] + " / " + taxonomydb[taxid]["class"] + " / " + taxonomydb[taxid]["order"] + " / " + taxonomydb[taxid]["family"] + " / " + taxonomydb[taxid]["genus"] + " / " + taxonomydb[taxid]["species"] + "\n"
 
+def get_kingdom(species, genus, family):
+    if species != "unknown":
+        searchWord = species
+    elif genus != "unknown":
+        searchWord = genus
+    elif family != "unknown":
+        searchWord = family
+    else:
+        return "unknown kingdom"
+    rec = requests.get("http://resolver.globalnames.org/name_resolvers.json", params={"names": searchWord}, allow_redirects=True)
+    if rec.text:
+        a = json.loads(rec.text)
+        globalNamesResult = {}
+        for x in a["data"][0]["results"]:
+            if x["data_source_title"] not in globalNamesResult:
+                globalNamesResult[x["data_source_title"]] = x
+        kingdom = "unknown kingdom"
+        if "Catalogue of Life" in globalNamesResult:
+            kingdom = globalNamesResult["Catalogue of Life"]["classification_path"].split("|")[0]
+        elif "GBIF Backbone Taxonomy" in globalNamesResult:
+            kingdom = globalNamesResult["GBIF Backbone Taxonomy"]["classification_path"].split("|")[0]
+        return kingdom
+    else:
+        return "unknown kingdom"
+
+def find_bold_taxonomy(line):
+    boldId = line.split("\t")[1].split("|")[1]
+    rec = requests.get("http://www.boldsystems.org/index.php/API_Public/specimen",params={"ids": boldId, "format":"json"}, allow_redirects=True)
+    if rec.text:
+        a = json.loads(rec.text)
+        if len(a["bold_records"]["records"]) == 1:
+            for x in a["bold_records"]["records"]:
+                try:
+                    phylum = a["bold_records"]["records"][x]["taxonomy"]["phylum"]["taxon"]["name"]
+                except KeyError:
+                    phylum = "unknown"
+                try:
+                    class_taxon = a["bold_records"]["records"][x]["taxonomy"]["class"]["taxon"]["name"]
+                except KeyError:
+                    class_taxon = "unknown"
+                try:
+                    order = a["bold_records"]["records"][x]["taxonomy"]["order"]["taxon"]["name"]
+                except KeyError:
+                    order = "unknown"
+                try:
+                    family = a["bold_records"]["records"][x]["taxonomy"]["family"]["taxon"]["name"]
+                except KeyError:
+                    family = "unknown"
+                try:
+                    genus = a["bold_records"]["records"][x]["taxonomy"]["genus"]["taxon"]["name"]
+                except KeyError:
+                    genus = "unknown"
+                try:
+                    species = a["bold_records"]["records"][x]["taxonomy"]["species"]["taxon"]["name"]
+                except KeyError:
+                    species = "unknown"
+        else:
+            return line.strip() + "\t" +"something went wrong, more than one record found\n"
+        kingdom = get_kingdom(species, genus, family)
+        taxonomy = " / ".join([kingdom, phylum, class_taxon, order, family, genus, species])
+        return line.strip() + "\t" + taxonomy+"\n"
+    else:
+        return line.strip() + "\tunknown kingdom / unknown phylum / unknown class / unknown order / unknown family / unknown genus / unknown species\n"
+
+def find_gbif_taxonomy(line):
+    splitLine = line.split("\t")
+    species = line.split("\t")[-1].split(" / ")[-1]
+    genus = line.split("\t")[-1].split(" / ")[-2]
+    if "unknown" not in species:
+        searchWord = species
+    elif "unknown" not in genus:
+        searchWord = genus
+    else:
+        return line
+
+    rec = requests.get("http://resolver.globalnames.org/name_resolvers.json", params={"names": searchWord}, allow_redirects=True)
+    if rec.text:
+        a = json.loads(rec.text)
+        globalNamesResult = {}
+        for x in a["data"][0]["results"]:
+            if x["data_source_title"] not in globalNamesResult:
+                globalNamesResult[x["data_source_title"]] = x
+        if "Catalogue of Life" in globalNamesResult:
+            taxonomy = globalNamesResult["Catalogue of Life"]["classification_path"].replace("|", " / ")
+        elif "GBIF Backbone Taxonomy" in globalNamesResult:
+            taxonomy = globalNamesResult["GBIF Backbone Taxonomy"]["classification_path"].replace("|", " / ")
+        else:
+            taxonomy = splitLine[-1]
+        splitLine[-1] = taxonomy
+        return "\t".join(splitLine)
+    else:
+        return line
 
 def add_taxonomy(file, taxonomyDict, mergedTaxonDict):
-    with open(file) as blasthits, open(args.blastinputfolder.strip() + "/taxonomy_"+ os.path.basename(file), "a") as output:
+    with open(file, "r") as blasthits, open(args.blastinputfolder.strip() + "/taxonomy_"+ os.path.basename(file), "a") as output:
+        line_taxonomy = ""
         for line in blasthits:
             if line.split("\t")[1].split("|")[0] == "BOLD":
-                pass
+                line_taxonomy = find_bold_taxonomy(line)
             else:
-                print find_genbank_taxonomy(line, taxonomyDict, mergedTaxonDict)
-            if args.taxonomy_source == "BGIF":
-                pass
-            if args.taxonomy_source == "default":
-                pass#write
+                line_taxonomy = find_genbank_taxonomy(line, taxonomyDict, mergedTaxonDict)
+            if args.taxonomy_source == "GBIF":
+                line_taxonomy = find_gbif_taxonomy(line_taxonomy)
+                output.write(line_taxonomy.strip()+"\n")
+            elif args.taxonomy_source == "default":
+                output.write(line_taxonomy)
 
 def process_files(taxonomyDict, mergedTaxonDict):
     files = [x for x in sorted(glob.glob(args.blastinputfolder.strip() + "/*.tabular"))]
