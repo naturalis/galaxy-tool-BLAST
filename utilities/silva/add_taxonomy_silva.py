4846602c0f035c62829517690c72763caf8d64c6
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-01-15 14:52:06 +0100

    Added argsparse

--- utilities/silva/add_taxonomy_silva.py
+++ utilities/silva/add_taxonomy_silva.py
@@ -3,9 +3,16 @@
 """
 from Bio import SeqIO
 
+# Retrieve the commandline arguments
+parser = argparse.ArgumentParser(description='Add taxonomy to silva')
+parser.add_argument('-f', dest='fasta', type=str, required=True)
+parser.add_argument('-t', dest='taxonomy', type=str, required=True)
+parser.add_argument('-o', dest='output', type=str, required=True)
+args = parser.parse_args()
+
 def make_taxon_dict():
     taxonDict = {}
-    with open("taxmap_slv_ssu_ref_132.txt") as silvaTaxonomy:
+    with open(args.taxonomy) as silvaTaxonomy:
         for x in silvaTaxonomy:
             x = x.strip().split("\t")
             accession = x[0]
@@ -17,7 +24,7 @@ def make_taxon_dict():
     return taxonDict
 
 def add_taxonomy(taxonDict):
-    with open("SILVA_132_SSURef_tax_silva.fasta", "r") as silva, open("SILVA_132_SSURef_tax_silva_taxonomy.fasta", "a") as output:
+    with open(args.fasta, "r") as silva, open(args.output, "a") as output:
         for record in SeqIO.parse(silva, "fasta"):
             accession = str(record.id).split(".")[0]
             #print accession
@@ -25,8 +32,7 @@ def add_taxonomy(taxonDict):
             output.write(newHeader+str(record.seq)+"\n")
 
 def main():
-    taxonDict = make_taxon_dict()
-    add_taxonomy(taxonDict)
+    add_taxonomy(make_taxon_dict())
 
 if __name__ == "__main__":
     main()
