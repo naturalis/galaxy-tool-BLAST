7f896bd9781dcb8263411c641a32528c31151512
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-02-18 15:50:20 +0100

    Automation Automation

--- utilities/bold/simplecheck_bold_taxonomy.sh
+++ utilities/bold/simplecheck_bold_taxonomy.sh
@@ -3,5 +3,5 @@ for f in bold_taxonomy_files/*.tsv
 do
 echo $f
 echo $f"\n" >> simplecheck_bold_taxonomy_log.txt
-grep "Fatal error:" $f >> simplecheck_bold_taxonomy_log.txt
+grep "Fatal error:" $f >> simplecheck_bold_taxonomy_log.txt || true
 done
