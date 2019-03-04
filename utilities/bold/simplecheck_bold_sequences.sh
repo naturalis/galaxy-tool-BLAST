7b23574e3e295060d4774b2dae48f7e8ecd27c10
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-03-04 16:54:54 +0100

    update bold pipeline

--- utilities/bold/simplecheck_bold_sequences.sh
+++ utilities/bold/simplecheck_bold_sequences.sh
@@ -1,7 +1,6 @@
-#change "bold_fasta_files" to the folder where you have your bold fastafiles
-for f in bold_fasta_files/*.fasta
+for f in $1"/"*".fasta"
 do
 echo $f
-echo $f"\n" >> simplecheck_bold_sequences_log.txt
-grep "<" $f >> simplecheck_bold_sequences_log.txt
+echo $f"\n" >> $2
+grep "<" $f >> $2
 done
--- utilities/bold/simplecheck_bold_taxonomy.sh
+++ utilities/bold/simplecheck_bold_taxonomy.sh
@@ -1,7 +1,6 @@
-#change "bold_taxonomy_files" to the folder where you have your bold taxonomy files
-for f in bold_taxonomy_files/*.tsv
+for f in $1"/"*".tsv"
 do
 echo $f
-echo $f"\n" >> simplecheck_bold_taxonomy_log.txt
-grep "Fatal error:" $f >> simplecheck_bold_taxonomy_log.txt || true
+echo $f"\n" >> $2
+grep "Fatal error:" $f >> $2 || true
 done
