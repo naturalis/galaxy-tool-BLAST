7b86d8957320e18bd395e5eee32c06a16fa2887d
MartenHoogeveen <martenhoogeveen@gmail.com>
2019-05-29 11:38:48 +0000

    bold output

--- utilities/bold/get_bold_sequences.sh
+++ utilities/bold/get_bold_sequences.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Obtain a list of all the phyla located on BOLD
 mkdir $1
-for sp in $(wget -O - -q http://www.barcodinglife.org/index.php/TaxBrowser_Home | grep taxid | grep -o "[0-9]\">.* " | cut -f2 -d ">")
+for sp in $(wget -O - http://www.barcodinglife.org/index.php/TaxBrowser_Home | grep taxid | grep -o "[0-9]\">.* " | cut -f2 -d ">")
 do
 	# Use wget to download the file from the BoLD API and append it to the temporary fasta file
 	echo $sp
--- utilities/bold/get_bold_taxonomy.sh
+++ utilities/bold/get_bold_taxonomy.sh
@@ -1,6 +1,6 @@
 # Obtain a list of all the Kingdoms located on BOLD
 mkdir $1
-for sp in $(wget -O - -q http://www.barcodinglife.org/index.php/TaxBrowser_Home | grep taxid | grep -o "[0-9]\">.* " | cut -f2 -d ">")
+for sp in $(wget -O - http://www.barcodinglife.org/index.php/TaxBrowser_Home | grep taxid | grep -o "[0-9]\">.* " | cut -f2 -d ">")
 do
 	# Use wget to download the file from the BoLD API and append it to the temporary fasta file
 	#echo $sp >> bold_log.txt
