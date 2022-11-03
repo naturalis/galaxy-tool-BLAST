dabf63df289f21ae33a025a2e96f550a9160e20e
dickgroenenberg <dickgroenenberg@gmail.com>
2022-11-03 15:50:49 +0100

    outlocation changed; sanity check added

--- blastn.sh
+++ blastn.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-echo $CONDA_DEFAULT_ENV
-python --version
 
-outlocation=$(mktemp -d /media/GalaxyData/database/files/XXXXXX)
+outlocation=$(mktemp -d /data/files/XXXXXX)
 SCRIPTDIR=$(dirname "$(readlink -f "$0")")
+
+# sanity check
+printf "Conda env: $CONDA_DEFAULT_ENV\n"
+printf "Outlocation: $outlocation\n"
+printf "Python version: $(python --version |  awk '{print $2}')\n"
+printf "Biopython version: $(conda list | egrep biopython | awk '{print $2}')\n"
+printf "Blastn version: $(blastn -version | head -n1 | awk '{print $2}')\n"
+printf "Unzip version: $(unzip -v | head -n1 | awk '{print $2}')\n"
+printf "Bash version: ${BASH_VERSION}\n"
+printf "SCRIPTDIR: $SCRIPTDIR\n\n"
 
 python $SCRIPTDIR"/blastn_wrapper.py" -it $1 -i $2 -db $3 -bt $4 -bm $5 -of $outlocation -outfmt $6 -cov "${10}" -id "${11}" -dbt "${13}"
 
@@ -25,7 +33,7 @@ then
 #below the code to call the script to add taxonomy and move the files to the galaxy output
 elif [ $6 == "custom_taxonomy" ] && [ "${9}" != "none" ]
 then
-    $SCRIPTDIR"/blastn_add_taxonomy.py" -i $outlocation'/files/' -t /extend/blast_databases/taxonomy/rankedlineage.dmp -m /extend/blast_databases/taxonomy/merged.dmp -ts "${9}" -taxonomy_db /extend/blast_databases/taxonomy/gbif_taxonmatcher
+    $SCRIPTDIR"/blastn_add_taxonomy.py" -i $outlocation'/files/' -t /data/blast_databases/taxonomy/rankedlineage.dmp -m /data/blast_databases/taxonomy/merged.dmp -ts "${9}" -taxonomy_db /data/blast_databases/taxonomy/gbif_taxonmatcher
     if [ $1 == "zip" ]
     then
         zip -r -j $outlocation"/blast_output.zip" $outlocation'/files/'*taxonomy_*'.tabular' --quiet
